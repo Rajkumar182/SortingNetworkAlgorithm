aedeccdb58c2635f901d1077dc6df07d5072a856
DELL <rajkumartamang182@gmail.com>
2021-12-24 16:08:59 +0545

    Second

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ document.getElementById("first-array-fifth-index").innerHTML = arr[5];
 //let arr =[a, b, c, d, e, f];
 document.write("<br>" + arr + "<br>");
 
-
 //Second Step Array Swap
 document.write("Second Step array swap" + "<br>");
 // Swapping element at index 0 and index 1
@@ -78,7 +77,6 @@ if (arr[4] > arr[5]) {
   document.write(arr + "<br>");
   document.getElementById("second-array-fourth-index").innerHTML = arr[4];
   document.getElementById("second-array-fifth-index").innerHTML = arr[5];
-
 } else {
   [arr[4], arr[5]] = [arr[4], arr[5]];
   document.write(arr + "<br>");
@@ -88,10 +86,6 @@ if (arr[4] > arr[5]) {
 // Print the array
 document.write(arr + "<br>");
 //Second Step Array Swap End
-
-
-
-
 
 //---------Third Step Array SWAP------------//
 document.write("Second Step Array Swap");
@@ -106,13 +100,13 @@ document.write("<br>" + arr);
 if (arr[0] > arr[1]) {
   [arr[0], arr[1]] = [arr[1], arr[0]];
   document.write("<br>" + arr + "<br>");
-  document.getElementById("Third-array-zero-index").innerHTML = arr[0];
-  document.getElementById("Third-array-first-index").innerHTML = arr[1];
+  document.getElementById("third-array-zero-index").innerHTML = arr[0];
+  document.getElementById("third-array-first-index").innerHTML = arr[1];
 } else {
   [arr[0], arr[1]] = [arr[0], arr[1]];
   document.write(arr + "<br>");
-  document.getElementById("Third-array-zero-index").innerHTML = arr[0];
-  document.getElementById("Third-array-first-index").innerHTML = arr[1];
+  document.getElementById("third-array-zero-index").innerHTML = arr[0];
+  document.getElementById("third-array-first-index").innerHTML = arr[1];
 }
 
 // Swapping element at index 2 and index 3
@@ -120,9 +114,13 @@ if (arr[0] > arr[1]) {
 if (arr[2] > arr[3]) {
   [arr[2], arr[3]] = [arr[3], arr[2]];
   document.write(arr + "<br>");
+  document.getElementById("third-array-second-index").innerHTML = arr[2];
+  document.getElementById("third-array-third-index").innerHTML = arr[3];
 } else {
   [arr[2], arr[3]] = [arr[2], arr[3]];
   document.write(arr + "<br>");
+  document.getElementById("third-array-second-index").innerHTML = arr[2];
+  document.getElementById("third-array-third-index").innerHTML = arr[3];
 }
 
 // Swapping element at index 4 and index 5
@@ -130,17 +128,18 @@ if (arr[2] > arr[3]) {
 if (arr[4] > arr[5]) {
   [arr[4], arr[5]] = [arr[5], arr[4]];
   document.write(arr + "<br>");
+  document.getElementById("third-array-fourth-index").innerHTML = arr[4];
+  document.getElementById("third-array-fifth-index").innerHTML = arr[5];
 } else {
   [arr[4], arr[5]] = [arr[4], arr[5]];
   document.write(arr + "<br>");
+  document.getElementById("third-array-fourth-index").innerHTML = arr[4];
+  document.getElementById("third-array-fifth-index").innerHTML = arr[5];
 }
 // Print the array
 document.write(arr + "<br>");
 
 //-------Third Step Array Swap End-----//
-
-
-
 
 //--------Fourth Step Array Swap-----------//
 document.write("Third Step Array Swap");
@@ -155,18 +154,26 @@ document.write("<br>" + arr + "<br>");
 if (arr[0] > arr[1]) {
   [arr[0], arr[1]] = [arr[1], arr[0]];
   document.write("<br>" + arr + "<br>");
+  document.getElementById("fourth-array-zero-index").innerHTML = arr[0];
+  document.getElementById("fourth-array-first-index").innerHTML = arr[1];
 } else {
   [arr[0], arr[1]] = [arr[0], arr[1]];
   document.write(arr + "<br>");
+  document.getElementById("fourth-array-zero-index").innerHTML = arr[0];
+  document.getElementById("fourth-array-first-index").innerHTML = arr[1];
 }
 
 // Swapping element at index 2 and index 3
 if (arr[2] > arr[3]) {
   [arr[2], arr[3]] = [arr[3], arr[2]];
   document.write(arr + "<br>");
+  document.getElementById("fourth-array-second-index").innerHTML = arr[2];
+  document.getElementById("fourth-array-third-index").innerHTML = arr[3];
 } else {
   [arr[2], arr[3]] = [arr[2], arr[3]];
   document.write(arr + "<br>");
+  document.getElementById("fourth-array-second-index").innerHTML = arr[2];
+  document.getElementById("fourth-array-third-index").innerHTML = arr[3];
 }
 
 // Swapping element at index 4 and index 5
@@ -174,21 +181,18 @@ if (arr[2] > arr[3]) {
 if (arr[4] > arr[5]) {
   [arr[4], arr[5]] = [arr[5], arr[4]];
   document.write(arr + "<br>");
+  document.getElementById("fourth-array-fourth-index").innerHTML = arr[4];
+  document.getElementById("fourth-array-fifth-index").innerHTML = arr[5];
 } else {
   [arr[4], arr[5]] = [arr[4], arr[5]];
   document.write(arr + "<br>");
+  document.getElementById("fourth-array-fourth-index").innerHTML = arr[4];
+  document.getElementById("fourth-array-fifth-index").innerHTML = arr[5];
 }
 // Print the array
 document.write(arr + "<br>");
 
 //--------Fourth Step Array Swap End-------//
-
-
-
-
-
-
-
 
 //------Fifth Step Swap Array Swap-------//
 
@@ -196,38 +200,58 @@ document.write("<br>" + "Fourth Step Swap" + "<br>");
 if (arr[1] > arr[2]) {
   [arr[1], arr[2]] = [arr[2], arr[1]];
   document.write(arr + "<br>");
+
+  document.getElementById("seventh-array-zero-index").innerHTML = arr[0];
+  document.getElementById("fifth-array-first-index").innerHTML = arr[1];
+  document.getElementById("fifth-array-second-index").innerHTML = arr[2];
+  document.getElementById("seventh-array-fifth-index").innerHTML = arr[5];
 } else {
   [arr[1], arr[2]] = [arr[1], arr[2]];
   document.write(arr);
+  document.getElementById("seventh-array-zero-index").innerHTML = arr[0];
+  document.getElementById("fifth-array-first-index").innerHTML = arr[1];
+  document.getElementById("fifth-array-second-index").innerHTML = arr[2];
+  document.getElementById("seventh-array-fifth-index").innerHTML = arr[5];
 }
 
 if (arr[3] > arr[4]) {
   [arr[3], arr[4]] = [arr[4], arr[3]];
   document.write(arr);
+
+  document.getElementById("fifth-array-first-index").innerHTML = arr[1];
+  document.getElementById("fifth-array-third-index").innerHTML = arr[3];
+  document.getElementById("fifth-array-fourth-index").innerHTML = arr[4];
 } else {
   [arr[3], arr[4]] = [arr[3], arr[4]];
   document.write(arr);
+  document.getElementById("fifth-array-third-index").innerHTML = arr[3];
+  document.getElementById("fifth-array-fourth-index").innerHTML = arr[4];
 }
 //-----------Fifth Step Array End----------//
-
-
-
-
-
-
 
 //-----------Sixth Step Array Swapping-----------------//
 document.write("<br>" + "fifth Step Array Swap" + "<br>");
 if (arr[2] > arr[3]) {
   [arr[2], arr[3]] = [arr[3], arr[2]];
   document.write(arr);
+  document.getElementById("sixth-array-second-index").innerHTML = arr[2];
+  document.getElementById("sixth-array-third-index").innerHTML = arr[3];
 } else {
   [arr[2], arr[3]] = [arr[2], arr[3]];
   document.write(arr);
+  document.getElementById("sixth-array-second-index").innerHTML = arr[2];
+  document.getElementById("sixth-array-third-index").innerHTML = arr[3];
 }
 
 document.write("<br>" + " Final sorted array is " + "<br>" + arr);
-//-----------Sixth Step Array Swap End------------//
+//-----------Seventh Step Array Swap End------------//
 
-document.getElementById("zero-index").innerHTML = arr[5];
-document.getElementById("first-index").innerHTML = arr[6];
+if (arr[0] < arr[1] < arr[2] < arr[3] < arr[4] < arr[5]) {
+  document.getElementById("seventh-array-zero-index").innerHTML = arr[0];
+  document.getElementById("seventh-array-first-index").innerHTML = arr[1];
+  document.getElementById("seventh-array-second-index").innerHTML = arr[2];
+  document.getElementById("seventh-array-third-index").innerHTML = arr[3];
+  document.getElementById("seventh-array-fourth-index").innerHTML = arr[4];
+  document.getElementById("seventh-array-fifth-index").innerHTML = arr[5];
+}
+console.log("Working");
